78b7593b9224c4d7ffa0b6962305aad5c99bd127
Simon JAILLET <sjaillet@gmail.com>
2015-10-12 23:02:14 +0200

    Initial commit

---
+++ spec/suite/CodeSpec.php
@@ -0,0 +1,133 @@
+<?php
+namespace code\spec\suite;
+
+use Exception;
+use InvalidArgumentException;
+use code\TimeoutException;
+use code\Code;
+
+describe("Code", function() {
+
+    declare(ticks = 1) {
+
+        describe("::run()", function() {
+
+            it("runs the passed closure", function () {
+
+                $start = microtime(true);
+
+                expect(Code::run(function() {return true;}, 1))->toBe(true);
+
+                $end = microtime(true);
+                expect($end - $start)->toBeLessThan(1);
+
+            });
+
+            it("throws an exception if an invalid closure is provided", function() {
+
+                $closure = function() {
+                    Code::run("invalid", 1);
+                };
+
+                expect($closure)->toThrow(new InvalidArgumentException());
+
+            });
+
+            it("throws an exception on timeout", function() {
+
+                $start = microtime(true);
+
+                $closure = function() {
+                    Code::run(function() {
+                        while(true) sleep(1);
+                    }, 1);
+                };
+
+                expect($closure)->toThrow(new TimeoutException('Timeout reached, execution aborted after 1 second(s).'));
+
+                $end = microtime(true);
+                expect($end - $start)->toBeGreaterThan(1);
+
+            });
+
+            it("throws all unexpected exceptions", function() {
+
+                $closure = function() {
+                    Code::run(function() {
+                        throw new Exception("Error Processing Request");
+                    }, 1);
+                };
+
+                expect($closure)->toThrow(new Exception("Error Processing Request"));
+
+            });
+
+            it("ignores exceptions when the third parameter is true", function() {
+
+                $closure = function() {
+                    Code::run(function() {
+                        throw new Exception("Error Processing Request");
+                    }, 1, true);
+                };
+
+                expect($closure)->not->toThrow(new Exception("Error Processing Request"));
+
+            });
+
+        });
+
+    }
+
+    describe("::spin()", function() {
+
+        it("runs the passed closure", function () {
+
+            $start = microtime(true);
+
+            expect(Code::spin(function() {return true;}, 1))->toBe(true);
+
+            $end = microtime(true);
+            expect($end - $start)->toBeLessThan(1);
+
+        });
+
+        it("throws an exception if an invalid closure is provided", function() {
+
+            $closure = function() {
+                Code::spin("invalid", 1);
+            };
+
+            expect($closure)->toThrow(new InvalidArgumentException());
+
+        });
+
+        it("throws an exception on timeout", function() {
+
+            $start = microtime(true);
+
+            $closure = function() {
+                Code::spin(function() {}, 1);
+            };
+
+            expect($closure)->toThrow(new TimeoutException('Timeout reached, execution aborted after 1 second(s).'));
+
+            $end = microtime(true);
+            expect($end - $start)->toBeGreaterThan(1);
+
+        });
+
+        it("ignores exceptions when the third parameter is true", function() {
+
+            $closure = function() {
+                Code::spin(function() {
+                    throw new Exception("Error Processing Request");
+                }, 1, true);
+            };
+
+            expect($closure)->not->toThrow(new Exception("Error Processing Request"));
+
+        });
+
+    });
+
+});
---
+++ src/Code.php
@@ -0,0 +1,77 @@
+<?php
+namespace code;
+
+use Exception;
+use InvalidArgumentException;
+
+class Code
+{
+    /**
+     * Executes a callable until a timeout is reached or the callable returns `true`.
+     *
+     * @param  Callable $callable The callable to execute.
+     * @param  integer  $timeout  The timeout value.
+     * @return mixed
+     */
+    public static function run($callable, $timeout = 0, $ignoreException = false)
+    {
+        if (!is_callable($callable)) {
+            throw new InvalidArgumentException();
+        }
+
+        $timeout = (integer) $timeout;
+
+        pcntl_signal(SIGALRM, function($signal) use ($timeout) {
+            throw new TimeoutException("Timeout reached, execution aborted after {$timeout} second(s).");
+        }, true);
+
+        pcntl_alarm($timeout);
+
+        $result = null;
+
+        try {
+            $result = $callable();
+        } catch (Exception $e) {
+            if (!$ignoreException) {
+                throw $e;
+            }
+        } finally {
+            pcntl_alarm(0);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Executes a callable in a loop until a timeout is reached or the callable returns `true`.
+     *
+     * @param  Callable $callable The callable to execute.
+     * @param  integer  $timeout  The timeout value.
+     * @return mixed
+     */
+    public static function spin($callable, $timeout = 0, $ignoreException = false)
+    {
+        if (!is_callable($callable)) {
+            throw new InvalidArgumentException();
+        }
+
+        $closure = function() use ($callable, $timeout) {
+
+            $timeout = (float) $timeout;
+            $result = false;
+            $start = microtime(true);
+
+            do {
+                if ($result = $callable()) {
+                    return $result;
+                }
+                $current = microtime(true);
+
+            } while ($current - $start < $timeout);
+
+            throw new TimeoutException("Timeout reached, execution aborted after {$timeout} second(s).");
+        };
+
+        return static::run($closure, $timeout, $ignoreException);
+    }
+}
